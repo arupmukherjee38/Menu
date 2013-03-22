1186d8f3aa18a36cf5360d916d5db37f9143415b
Arup Mukherjee <arupmukherjee38@gmail.com>
2013-03-22 20:53:04 +0530

    dgbdbd
    dfgadfba

--- gen/com/example/ugcsap2013/R.java
+++ gen/com/example/ugcsap2013/R.java
@@ -24,13 +24,14 @@ public final class R {
         public static final int ugc=0x7f02000a;
     }
     public static final class id {
-        public static final int accessGraphe=0x7f070001;
-        public static final int accessGraphe2=0x7f070002;
-        public static final int list_image=0x7f070004;
-        public static final int menu_settings=0x7f070006;
+        public static final int Time=0x7f070001;
+        public static final int accessGraphe=0x7f070002;
+        public static final int accessGraphe2=0x7f070003;
+        public static final int list_image=0x7f070005;
+        public static final int menu_settings=0x7f070007;
         public static final int spinner1=0x7f070000;
-        public static final int thumbnail=0x7f070003;
-        public static final int title=0x7f070005;
+        public static final int thumbnail=0x7f070004;
+        public static final int title=0x7f070006;
     }
     public static final class layout {
         public static final int lectures=0x7f030000;
