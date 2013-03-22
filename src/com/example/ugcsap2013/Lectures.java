f222122af3420db5ee614b490711e0ca80853a5b
Arup Mukherjee <arupmukherjee38@gmail.com>
2013-03-22 21:10:14 +0530

    new
    new

--- src/com/example/ugcsap2013/Lectures.java
+++ src/com/example/ugcsap2013/Lectures.java
@@ -1,11 +1,17 @@
 package com.example.ugcsap2013;
 
 import android.app.Activity;
+import android.graphics.AvoidXfermode.Mode;
+import android.graphics.LightingColorFilter;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class Lectures extends Activity{
+	
+	
+	Button time;
 	
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +22,9 @@ public class Lectures extends Activity{
                     android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        time=(Button)findViewById(R.id.Time);
+        time.getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFFFFFFF));
+        
 		
     }
 	
