fadcd72007ef9eeb6e39691ac35daef606886ed9
Arup Mukherjee <arupmukherjee38@gmail.com>
2013-03-22 22:04:32 +0530

    Xmalready
    Xmlredy

--- src/com/example/ugcsap2013/Lectures.java
+++ src/com/example/ugcsap2013/Lectures.java
@@ -11,7 +11,7 @@ import android.widget.Spinner;
 public class Lectures extends Activity{
 	
 	
-	Button time;
+	Button time,date;
 	
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +24,20 @@ public class Lectures extends Activity{
         spinner.setAdapter(adapter);
         time=(Button)findViewById(R.id.Time);
         time.getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFFFFFFF));
+        date=(Button)findViewById(R.id.Date);
+        date.getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFFFFFFF));
+        String[] items2 = new String[] {"Nationl", "States", "University","College","International"};
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, items2);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        String[] items3 = new String[] {"Aloweted", "Extra"};
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, items3);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
         
 		
     }
