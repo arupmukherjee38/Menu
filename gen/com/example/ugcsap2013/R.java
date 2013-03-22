34bac11ef93900ec0700d1c0e8a8b1f0ad8702ba
Arup Mukherjee <arupmukherjee38@gmail.com>
2013-03-23 00:10:53 +0530

    Xml
    Xml

--- gen/com/example/ugcsap2013/R.java
+++ gen/com/example/ugcsap2013/R.java
@@ -26,16 +26,17 @@ public final class R {
     public static final class id {
         public static final int Date=0x7f070002;
         public static final int Time=0x7f070001;
-        public static final int accessGraphe=0x7f070006;
-        public static final int accessGraphe2=0x7f070007;
+        public static final int accessGraphe=0x7f070007;
+        public static final int accessGraphe2=0x7f070008;
         public static final int autoCompleteTextView1=0x7f070003;
-        public static final int list_image=0x7f070009;
-        public static final int menu_settings=0x7f07000b;
+        public static final int autoCompleteTextView2=0x7f070006;
+        public static final int list_image=0x7f07000a;
+        public static final int menu_settings=0x7f07000c;
         public static final int spinner1=0x7f070000;
         public static final int spinner2=0x7f070004;
         public static final int spinner3=0x7f070005;
-        public static final int thumbnail=0x7f070008;
-        public static final int title=0x7f07000a;
+        public static final int thumbnail=0x7f070009;
+        public static final int title=0x7f07000b;
     }
     public static final class layout {
         public static final int lectures=0x7f030000;
--- src/com/example/ugcsap2013/Lectures.java
+++ src/com/example/ugcsap2013/Lectures.java
@@ -4,15 +4,22 @@ import android.app.Activity;
 import android.graphics.AvoidXfermode.Mode;
 import android.graphics.LightingColorFilter;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class Lectures extends Activity{
+public class Lectures extends Activity implements TextWatcher{
 	
 	
 	Button time,date;
-	
+	AutoCompleteTextView myAutoComplete,myAutoComplete1;
+	String item[]={
+	  "cse", "it", "mca", "Ece",
+	  
+	};
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lectures);
@@ -38,9 +45,32 @@ public class Lectures extends Activity{
                     android.R.layout.simple_spinner_item, items3);
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner3.setAdapter(adapter3);
+        myAutoComplete = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
         
+        myAutoComplete.addTextChangedListener(this);
+        myAutoComplete.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, item));
+        myAutoComplete1 = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView2);
+        
+        myAutoComplete1.addTextChangedListener(this);
+        myAutoComplete1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, item));
 		
     }
+	@Override
+	public void afterTextChanged(Editable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+			int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
